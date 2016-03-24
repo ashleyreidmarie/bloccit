1865466f7182fb8763de7c4941c44e7bbbd96991
Ashley Reid <ashleyreidmarie@gmail.com>
2016-03-24 03:10:28 +0000

    Checkpoint 39: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  #before_save { self.name = self.name.split(" ").map(&:capitalize).join(" ") if name }
+  has_many :posts
+  
+  before_save { self.name = self.name.split(" ").map(&:capitalize).join(" ") if name }
   before_save { self.email = email.downcase }
 
   
@@ -7,7 +9,7 @@ class User < ActiveRecord::Base
   
   validates :password, presence: true, length: { minimum: 6}, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
-  
+  ;
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
