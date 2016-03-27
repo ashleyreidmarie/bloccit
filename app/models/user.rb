c048c7c1e69d7ab80698a62df412895ef931907e
Ashley Reid <ashleyreidmarie@gmail.com>
2016-03-27 00:51:36 +0000

    Checkpoint 40: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   
   before_save { self.name = self.name.split(" ").map(&:capitalize).join(" ") if name }
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
   
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -16,4 +17,6 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
             
   has_secure_password
+  
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -77,10 +77,18 @@ end
 #     )
 # end
 
-#Create static test user
-user = User.first
-user.update_attributes!(
-  email: 'ashleyreidmarie@gmail.com',
+#Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+
+#Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
   )
     
